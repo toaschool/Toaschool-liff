acbab87fcd35b593946f664cf4cf4cf17d2c085b
toaschool <toaschoolmail@gmail.com>
2026-06-26 12:10:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    ============================================================ */
 
 // URL /exec ของ Web App (จาก Apps Script > Deploy > Web app)
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbz-RBZQXhupciK9zOPpYdtbwDtNlKBH38Qzn9abJPvpJqFx2d0M782CdYzOHIM5ii7-ag/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzhek51o3b15VTP7MH4v2ZKe7cnh-d0HmZpvRPxWp_z6xD0K-wNXAwM9bFZgiHBr5JH/exec";
 
 // LIFF ID (จาก LINE Developers > LIFF > สร้าง LIFF app ชี้มาที่หน้านี้)
 const LIFF_ID = "";
